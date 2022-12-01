202fb3d23128ee2b25b51a9d1ae23db97cd66f5e
afrinjamanbd <mailafrinjaman@gmail.com>
2022-12-01 21:22:52 +0600

    First commit

---
+++ python_class3.py
@@ -0,0 +1,15 @@
+
+# myinput = input('Please enter:').split()
+# print(myinput)
+
+
+mylist = [1, 4, 6, 34, 79, 345, 456357, 365457434]
+newlist = [22340, 32410, 35464, 425, 239]
+newvar = 0
+
+for a in mylist:
+
+    
+print(newvar)
+
+
